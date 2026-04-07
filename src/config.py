3e8d0f02d3a239d0c87d76b0fcf5417d5dc41fdf
Rasmus Kankkunen <rasmus.kankkunen@tuni.fi>
2026-04-07 11:24:02 +0300

    Created a config file to be used universaily through out all projects

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# Parameters for sampling and windowing.
+FS = 2000           # Hz
+WINDOW_MS = 200     # Milliseconds
+STEP_MS = 100       # Milliseconds
+N_CHANNELS = 12
+N_CLASSES = 18
+
+# Filter parameters
+LOWCUT = 20         # Hz
+HIGHCUT = 500       # Hz
+FILTER_ORDER = 4

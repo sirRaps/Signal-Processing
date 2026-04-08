6f6da1db86ab3d40eb78f7229452a27206bf80eb
Rasmus Kankkunen <rasmus.kankkunen@tuni.fi>
2026-04-08 22:14:25 +0300

    added subject datapath fetching to config to be universal

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+import os
+
 # Parameters for sampling and windowing.
 FS = 2000           # Hz
 WINDOW_MS = 200     # Milliseconds
@@ -9,3 +11,23 @@ N_CLASSES = 18
 LOWCUT = 20         # Hz
 HIGHCUT = 500       # Hz
 FILTER_ORDER = 4
+
+# Update this to your USB mount path
+USB_PATH = '/Volumes/KINGSTON'
+DATA_PATH = os.path.join(USB_PATH, 'ninapro_db2')
+N_SUBJECTS = 40
+
+def get_subject_path(subject_id, exercise=1):
+    """
+    Get path to a Ninapro DB2 subject file.
+    
+    Args:
+        subject_id: integer 1-40
+        exercise: 1, 2, or 3 (default 1)
+    
+    Returns:
+        Full path to .mat file
+    """
+    folder = f'DB2_s{subject_id}'
+    filename = f'S{subject_id}_E{exercise}_A1.mat'
+    return os.path.join(DATA_PATH, folder, filename)
